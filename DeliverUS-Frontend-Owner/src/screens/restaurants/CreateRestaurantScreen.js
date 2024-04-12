ae52a344aba3a15ed8b68670792e963c0ad890d2
Julián Romero Parejo <114395879+julrompar@users.noreply.github.com>
2024-04-12 19:19:05 +0200

    Lab 6 (CreateRestaurants Form)

--- DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
+++ DeliverUS-Frontend-Owner/src/screens/restaurants/CreateRestaurantScreen.js
@@ -1,16 +1,164 @@
-/* eslint-disable react/prop-types */
-import React from 'react'
-import { StyleSheet } from 'react-native'
+import React, { useEffect, useState } from 'react'
+import { Image, Platform, Pressable, ScrollView, StyleSheet, View } from 'react-native'
+import * as ExpoImagePicker from 'expo-image-picker'
+import { MaterialCommunityIcons } from '@expo/vector-icons'
+
+import { getRestaurantCategories } from '../../api/RestaurantEndpoints'
+import InputItem from '../../components/InputItem'
 import TextRegular from '../../components/TextRegular'
+import * as GlobalStyles from '../../styles/GlobalStyles'
+import restaurantLogo from '../../../assets/restaurantLogo.jpeg'
+import restaurantBackground from '../../../assets/restaurantBackground.jpeg'
+import { showMessage } from 'react-native-flash-message'
+import { Formik } from 'formik'
 
 export default function CreateRestaurantScreen () {
+  const initialRestaurantValues = { name: null, description: null, address: null, postalCode: null, url: null, shippingCosts: null, email: null, phone: null, restaurantCategoryId: null }
+
+  useEffect(() => {
+    (async () => {
+      if (Platform.OS !== 'web') {
+        const { status } = await ExpoImagePicker.requestMediaLibraryPermissionsAsync()
+        if (status !== 'granted') {
+          alert('Sorry, we need camera roll permissions to make this work!')
+        }
+      }
+    })()
+  }, [])
+
+  const pickImage = async (onSuccess) => {
+    const result = await ExpoImagePicker.launchImageLibraryAsync({
+      mediaTypes: ExpoImagePicker.MediaTypeOptions.Images,
+      allowsEditing: true,
+      aspect: [1, 1],
+      quality: 1
+    })
+    if (!result.canceled) {
+      if (onSuccess) {
+        onSuccess(result)
+      }
+    }
+  }
+
   return (
-    <TextRegular textStyle={styles.text}>
-      Create restaurant
-    </TextRegular>
+    <Formik
+    initialValues={initialRestaurantValues}
+    >
+      {({ setFieldValue, values }) => (
+        <ScrollView>
+          <View style={{ alignItems: 'center' }}>
+            <View style={{ width: '60%' }}>
+              <InputItem
+                name='name'
+                label='Name:'
+              />
+              <InputItem
+                name='description'
+                label='Description:'
+              />
+              <InputItem
+                name='address'
+                label='Address:'
+              />
+              <InputItem
+                name='postalCode'
+                label='Postal code:'
+              />
+              <InputItem
+                name='url'
+                label='Url:'
+              />
+              <InputItem
+                name='shippingCosts'
+                label='Shipping costs:'
+              />
+              <InputItem
+                name='email'
+                label='Email:'
+              />
+              <InputItem
+                name='phone'
+                label='Phone:'
+              />
+
+              <Pressable onPress={() =>
+                pickImage(
+                  async result => {
+                    await setFieldValue('logo', result)
+                  }
+                )
+              }
+                style={styles.imagePicker}
+              >
+                <TextRegular>Logo: </TextRegular>
+                <Image style={styles.image} source={values.logo ? { uri: values.logo.assets[0].uri } : restaurantLogo} />
+              </Pressable>
+
+              <Pressable onPress={() =>
+                pickImage(
+                  async result => {
+                    await setFieldValue('heroImage', result)
+                  }
+                )
+              }
+                style={styles.imagePicker}
+              >
+                <TextRegular>Hero image: </TextRegular>
+                <Image style={styles.image} source={values.heroImage ? { uri: values.heroImage.assets[0].uri } : restaurantBackground} />
+              </Pressable>
+
+              <Pressable
+                onPress={() => console.log('Submit pressed')}
+                style={({ pressed }) => [
+                  {
+                    backgroundColor: pressed
+                      ? GlobalStyles.brandSuccessTap
+                      : GlobalStyles.brandSuccess
+                  },
+                  styles.button
+                ]}>
+              <View style={[{ flex: 1, flexDirection: 'row', justifyContent: 'center' }]}>
+                <MaterialCommunityIcons name='content-save' color={'white'} size={20}/>
+                <TextRegular textStyle={styles.text}>
+                  Save
+                </TextRegular>
+              </View>
+              </Pressable>
+            </View>
+          </View>
+        </ScrollView>
+      )}
+    </Formik>
   )
 }
 
 const styles = StyleSheet.create({
-
-})
+  button: {
+    borderRadius: 8,
+    height: 40,
+    padding: 10,
+    width: '100%',
+    marginTop: 20,
+    marginBottom: 20
+  },
+  text: {
+    fontSize: 16,
+    color: 'white',
+    textAlign: 'center',
+    marginLeft: 5
+  },
+  imagePicker: {
+    height: 40,
+    paddingLeft: 10,
+    marginTop: 20,
+    marginBottom: 80
+  },
+  image: {
+    width: 100,
+    height: 100,
+    borderWidth: 1,
+    alignSelf: 'center',
+    marginTop: 5
+  }
+}
+)
